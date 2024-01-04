f98a3cb89abd29d8b96f1f3a67319801ff8f1110
dosepehr <dosepehr@gmail.com>
2024-01-04 21:41:29 +0330

    edit host page styles

--- src/app/host/page.tsx
+++ src/app/host/page.tsx
@@ -1,11 +1,13 @@
 import Breadcrumb from '@/src/components/modules/breadcrumb/Breadcrumb';
 import Navbar from '@/src/components/modules/navbar/Navbar';
 import React from 'react';
-import HostCart from '@/src/components/modules/host/HostCart';
-import HostCartData from '@/src/constants/HostCartData';
+import HostCart from '@/src/components/modules/hostCart/HostCart';
 import Footer from '@/src/components/modules/Footer/Footer';
 import Button from '@/src/components/modules/button';
+import useHostCartData from '@/src/hooks/useHostCartData';
+
 const page = () => {
+    const HostData = useHostCartData();
     return (
         <>
             <Navbar />
@@ -14,8 +16,12 @@ const page = () => {
                 description='نکات زیر را مطالعه کنید تا با اطلاعات کافی در جاجیگا میزبان شوید و با خیال آسوده کسب درآمد کنید'
             />
 
-            {HostCartData.map((cartData) => (
-                <div className='py-4 space-y-10'>
+            {HostData.map((cartData) => (
+                <div
+                    key={cartData.id}
+                    className='py-4 space-y-10 rounded-t-xl'
+                    style={{ background: cartData.bgColor }}
+                >
                     <div className='flex flex-col text-center items-center justify-center space-y-2 rounded-t-xl'>
                         <h2 className='text-lg font-medium'>
                             {cartData.mainTitle}
@@ -25,14 +31,14 @@ const page = () => {
                             {cartData.secondaryTitle}
                         </p>
                     </div>
-                    <div className='flex flex-col md:flex-row items-center justify-center Container md:gap-x-4 gap-y-10 px-4 py-10'>
+                    <div className='flex flex-col md:flex-row items-start Container md:gap-x-4 gap-y-10 px-4 py-10'>
                         {cartData.carts.map((cart) => (
-                            <HostCart {...cart} />
+                            <HostCart {...cart} borderColor={cartData.bgColor} />
                         ))}
                     </div>
                 </div>
             ))}
-            <div className='mx-auto w-80 max-w-[320px] sticky bottom-2 bg-[#00000099] h-16 rounded-lg flex items-center justify-center px-4 mb-4'>
+            <div className='mx-auto w-80 max-w-[320px] sticky bottom-2 bg-[#00000099] h-16 rounded-lg flex items-center justify-center px-4 my-4'>
                 <Button
                     className='w-full p-2 rounded-full text-center text-black flex items-center justify-center hover:bg-[#d2b43d] duration-300'
                     variant='yellow'
---
+++ src/components/modules/hostCart/HostCart.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+interface HostCartProps {
+    title: string;
+    description: string;
+    icon: React.JSX.Element;
+    borderColor: string;
+}
+const HostCart: FC<HostCartProps> = ({
+    title,
+    description,
+    icon,
+    borderColor,
+}) => {
+    return (
+        <div
+            className='relative rounded-md border-t-[2px] border-yellow-300 flex flex-col items-center justify-center space-y-2 bg-white p-4 h-full'
+            style={{
+                boxShadow: 'rgba(160, 160, 160, 0.7) 0px 3px 10px',
+            }}
+        >
+            <div
+                className='rounded-full p-[2px] -top-8 absolute'
+                style={{
+                    background: `${`linear-gradient(0deg, rgb(255, 200, 4) 0%, rgb(255, 200, 4) 50%, ${borderColor} 50%, ${borderColor} 100%)`}`,
+                }}
+            >
+                <p
+                    className={`rounded-full bg-yellow-300 border-[3px] text-4xl p-2`}
+                    style={{ borderColor: `${borderColor}` }}
+                >
+                    {icon}
+                </p>
+            </div>
+            <div className='py-4'>
+                <h3 className='mb-4 font-medium'>{title}</h3>
+                <p className='text-sm text-textGray font-vazir-light'>
+                    {description}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default HostCart;
