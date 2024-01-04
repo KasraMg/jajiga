8a19d81a10dc945792d63c38e2533f75d57b4e42
dosepehr <dosepehr@gmail.com>
2024-01-05 01:55:27 +0330

    edit footer styles

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
 import AccessLinks from './components/AccessLinks';
 import MarketLinks from './components/MarketLinks';
 import Link from 'next/link';
 import { LiaInstagram, LiaTelegram } from 'react-icons/lia';
 import { PiTelegramLogoBold } from 'react-icons/pi';
-import { FaInstagram } from "react-icons/fa";
+import { FaInstagram } from 'react-icons/fa';
 import TrustUs from './components/TrustUs';
+import CitiesList from './components/CitiesList';
 const Footer = () => {
     return (
         <div className='bg-[#f1f1f1] p-4 rounded-2xl'>
             <div className='Container mx-auto'>
-                <div className='flex'>
+                <div className='flex flex-col md:flex-row'>
                     <div className='flex flex-col'>
-                        <div className='flex'>
-                            <div className='w-1/3 bg-pink-500'>
+                        <div className='flex flex-col sm:flex-row'>
+                            <div className='sm:w-1/2 md:w-1/3'>
                                 <p className='text-center mb-3'>
                                     لینک های دسترسی
                                 </p>
@@ -56,73 +56,67 @@ const Footer = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className='w-1/3 bg-blue-500'>
+                            <div className='sm:w-1/2 md:w-1/3 px-6 lg:px-0'>
                                 <p className='text-center mb-3'>
                                     نصب اپلیکیشن جاجیگا
                                 </p>
-                                <div className='flex items-center'>
-                                    <div className='flex items-center flex-col space-y-1 ml-2'>
-                                        <MarketLinks src='/images/markets/Bazzar.png' />
-                                        <MarketLinks src='/images/markets/Myket.png' />
-                                    </div>
-                                    <div className='flex flex-col items-center space-y-1'>
-                                        <MarketLinks src='/images/markets/PlayStore.png' />
-                                        <MarketLinks src='/images/markets/WebApp.png' />
-                                    </div>
+                                <div className='grid grid-cols-2 md:grid-cols-1 gap-x-4 gap-y-2 lg:grid-cols-2'>
+                                    <MarketLinks src='/images/markets/Bazzar.png' />
+                                    <MarketLinks src='/images/markets/Myket.png' />
+                                    <MarketLinks src='/images/markets/PlayStore.png' />
+                                    <MarketLinks src='/images/markets/WebApp.png' />
                                 </div>
                             </div>
-                            <div className='w-1/3 bg-red-500'>
+                            <div className='md:w-1/3 hidden md:block'>
                                 <p className='text-center mb-3'>
                                     با ما همراه شوید
                                 </p>
-                                <div className='flex flex-col space-y-1'>
-                                    <Link
-                                        href='/'
-                                        className='instagramGradiant w-1/2 mx-auto rounded-lg text-white flex items-center justify-center px-2'
-                                    >
-                                        <p>700K</p>
-                                        <FaInstagram className='text-3xl mr-4' />
-                                    </Link>
-                                    <Link
-                                        href='/'
-                                        className='telegramGradiant  w-1/2 mx-auto rounded-lg text-white flex items-center justify-center px-2'
-                                    >
-                                        <p>20K</p>
-                                        <LiaTelegram className='text-3xl mr-4' />
-                                    </Link>
-                                </div>
+                                <Link
+                                    href='/'
+                                    className='instagramGradiant mx-auto rounded-lg text-white flex items-center justify-center w-1/2 py-2 min-w-[150px]'
+                                >
+                                    <p>700K</p>
+                                    <FaInstagram className='text-3xl mr-4' />
+                                </Link>
+                                <Link
+                                    href='/'
+                                    className='telegramGradiant mx-auto rounded-lg text-white flex items-center justify-center w-1/2 py-2 min-w-[150px] mt-4'
+                                >
+                                    <p>20K</p>
+                                    <LiaTelegram className='text-3xl mr-4' />
+                                </Link>
                             </div>
                         </div>
-                        <div className='grid grid-cols-3 mt-4 bg-green-500'>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                            <p>اجاره ویلا شمال</p>
-                        </div>
+                        <CitiesList />
                     </div>
-                    <div className='bg-yellow-500'>
-                        <p>با خیال راحت به جاجیگا اعتماد کنید</p>
-                        <div className='grid grid-cols-2 gap-4'>
-                            <TrustUs />
-                            <TrustUs />
-                            <TrustUs />
-                            <TrustUs />
+                    <div className='flex flex-col sm:flex-row'>
+                        <div className=''>
+                            <p className='text-center mb-3'>
+                                با خیال راحت به جاجیگا اعتماد کنید
+                            </p>
+                            <div className='grid grid-cols-4 md:grid-cols-2 gap-4'>
+                                <TrustUs />
+                                <TrustUs />
+                                <TrustUs />
+                                <TrustUs />
+                            </div>
+                        </div>
+                        <div className='md:w-1/3 md:hidden'>
+                            <p className='text-center mb-3'>با ما همراه شوید</p>
+                            <Link
+                                href='/'
+                                className='instagramGradiant mx-auto rounded-lg text-white flex items-center justify-center w-1/2 py-2 min-w-[150px]'
+                            >
+                                <p>700K</p>
+                                <FaInstagram className='text-3xl mr-4' />
+                            </Link>
+                            <Link
+                                href='/'
+                                className='telegramGradiant mx-auto rounded-lg text-white flex items-center justify-center w-1/2 py-2 min-w-[150px] mt-4'
+                            >
+                                <p>20K</p>
+                                <LiaTelegram className='text-3xl mr-4' />
+                            </Link>
                         </div>
                     </div>
                 </div>
