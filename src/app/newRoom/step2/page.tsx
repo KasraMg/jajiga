fa00c777518b7c9ca9d54512acf252be4af6da1a
KasraMg <kasragithub7@gmail.com>
2024-01-19 19:20:39 +0330

    start step2

--- src/app/newRoom/step2/page.tsx
+++ src/app/newRoom/step2/page.tsx
@@ -1,5 +1,7 @@
 "use client"
-import Layout from '@/src/components/modules/Layout/Layout'; 
+import Layout from '@/src/components/modules/Layout/Layout';
+import { Map } from '@/src/components/modules/Map/Map';
+import Alert from '@/src/components/modules/alert/Alert';
 import Breadcrumb from '@/src/components/modules/breadcrumb/Breadcrumb';
 import Container from '@/src/components/modules/container/Container';
 import ContentNavigator from '@/src/components/modules/contentNavigator/ContentNavigator';
@@ -7,59 +9,35 @@ import Stepper from '@/src/components/modules/stepper/Stepper';
 import StepperInfo from '@/src/components/modules/stepperInfo/StepperInfo';
 import useStateData from '@/src/hooks/useStateData';
 import { useEffect, useState } from 'react'; 
-import Select from 'react-select';
 
 const stateOptions = useStateData()
 
 
 const page = () => {
-    const [textAreaValue,settextAreaValue]=useState<string>()
-    const [stateSelectedOption, setStateSelectedOption] = useState<{ label: string; value: string[]; } | null>(null);
-    const [citySelectedOption, setCitySelectedOption] = useState<{ label: string; value: string[]; } | null>(null);
-    const [cityOption, setCityOption] = useState<{ label: string, value: string[]}[]>([]);
-    const [citySelectorDisabel, setcitySelectorDisabel] = useState(true);
-    const [isLodaing, setIsLodaing] = useState(false)
-    const [disabelNextButton,setDisabelNextButton]=useState(true)
+   
+    const [disabelNextButton, setDisabelNextButton] = useState(true)
 
-    useEffect(() => {
-        setCitySelectedOption(null)
-        setIsLodaing(true)
-        if (stateSelectedOption?.value) {
-            const city: any =
-                stateSelectedOption?.value.map(data => {
-                    return {
-                        value: data,
-                        label: data
-                    }
-                })
-            setCityOption(city)
-            setcitySelectorDisabel(false)
-            setIsLodaing(false)
-        }
-
-    }, [stateSelectedOption])
-
-    useEffect(() => {
-      if (stateSelectedOption&&textAreaValue&&citySelectedOption) {
-        setDisabelNextButton(false)
-      }else(
-        setDisabelNextButton(true)
-      )
-    }, [stateSelectedOption,textAreaValue,citySelectedOption])
-    
-    const clickHandler=()=>{ 
+ 
+    const clickHandler = () => {
     }
     return (
         <Container disableFooter={true}>
             <Breadcrumb route={'ثبت اقامتگاه'} />
             <Layout>
-                <div className='max-w-[1120px] py-8  flex gap-5'>
-                    <Stepper active={1} />
-                    <div className='flex flex-col gap-8'>
-                        
-                       <ContentNavigator clickHandler={clickHandler} disablelPrevButton={true} disabelNextButton={disabelNextButton} prevLink={'/'} nextLink={'newRoom/step2'}/>
+                <div className='max-w-[1120px] py-8 flex gap-5'>
+                    <Stepper active={2} />
+                    <div className='flex flex-col gap-3'>
+                        <p>انتخاب موقعیت در نقشه</p>
+                        <p className='font-vazir-light text-sm leading-6'>مکان نما در وسط صفحه ثابت است. نقشه را به گونه ای حرکت دهیه تا مکان نما بر روی مکان اقامتگاه شما قرار گیرد. با استفاده از کلید + بر روی نقشه زوم کنید.</p>
+                        <Alert variant={"danger"} />
+                        <Map position={[35.551066,51.297588]}/>
+                        <ContentNavigator clickHandler={clickHandler} disablelPrevButton={true} disabelNextButton={disabelNextButton} prevLink={'/'} nextLink={'newRoom/step2'} />
                     </div>
-                    <StepperInfo title="آدرس اقامتگاه" text="آدرس دقیق اقامتگاه, تنها پس ازقطعی شدن رزروبرای میهمان ارسال می گردد." />
+                    <div className='max-w-[243px]'>
+                    <StepperInfo title="مکان اقامتگاه" text="با حرکت مکان نما و جابجا شدن بر روی نقشه, مکان نما را بر روی محل اقامتگاه خود, قرار دهید.
+                     همچنین می توانید با استفاده از کلیدهای + و – بزرگنمایی نقشه را تغییر دهید و یا برای دقت بیشتر, نقشه را به حالت Satellite درآورید." />
+                    </div>
+                  
                 </div>
             </Layout>
 
