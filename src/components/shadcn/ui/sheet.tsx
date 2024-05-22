d38586623c1b2915f6b265bedcbaa2cee7a605fd
KasraMg <kasragithub7@gmail.com>
2024-05-22 16:38:49 +0330

    add shadcn

--- src/components/shadcn/ui/sheet.tsx
+++ src/components/shadcn/ui/sheet.tsx
@@ -2,9 +2,7 @@
 
 import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
-import { cva, type VariantProps } from "class-variance-authority"
-import { X } from "lucide-react"
-
+import { cva, type VariantProps } from "class-variance-authority" 
 import { cn } from "@/lib/utils"
 
 const Sheet = SheetPrimitive.Root
