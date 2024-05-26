224c64d391d0593129a283a6f3717eddcce38ee4
KasraMg <kasragithub7@gmail.com>
2024-05-26 15:09:25 +0330

    fix new room stepper bug

--- src/components/modules/stepper/Stepper.tsx
+++ src/components/modules/stepper/Stepper.tsx
@@ -2,9 +2,7 @@
 import Link from 'next/link'
 import React, { FC, useState } from 'react'
 import { GiSandsOfTime } from 'react-icons/gi'
-import { IoCheckmark } from 'react-icons/io5'
-import { Accordion, AccordionBody, AccordionHeader } from '@material-tailwind/react'
-
+import { IoCheckmark } from 'react-icons/io5'  
 interface StepperProps {
     active: number,
     className?: string
@@ -14,9 +12,8 @@ interface StepperData {
     id: number
 }
 const Stepper: FC<StepperProps> = ({ active, className }) => {
-    const [open, setOpen] = useState(1);
-
-    const handleOpen = (value: number) => setOpen(open === value ? 0 : value);
+    const [showSteps, setShowSteps] = useState(false);
+ 
     const [data, setData] = useState<StepperData[]>([
         { title: 'آدرس', id: 1 },
         { title: 'نقشه', id: 2 },
@@ -46,25 +43,25 @@ const Stepper: FC<StepperProps> = ({ active, className }) => {
                 ))}
             </ul>
 
-            <Accordion placeholder={null} open={open === 1} className=' z-10 md:!hidden block' id={1 as any}>
-                <AccordionHeader className='border-0' placeholder={null} onClick={() => handleOpen(1)}>
-                    <ul id='stepper2' className={`${open !== 1 ? '!h-[420px] !overflow-y-scroll' : 'h-16'} w-full flex flex-col gap-6 overflow-hidden  pr-0 p-4`}>
+            <div  className='z-10 md:!hidden block' id={1 as any}>
+                <div className='border-0 ml-4'>
+                    <ul  onClick={() => setShowSteps(prev => !prev)} style={{transition:'.1s all ease-in'}} id='stepper2' className={`${showSteps ? '!h-[420px] !overflow-y-scroll' : 'h-16'} w-full flex flex-col gap-6 overflow-y-scroll relative px-0 p-4`}>
                         {data && data.map(stepper => (
                             <Link href={`${active >= stepper.id && `/newRoom/step` + stepper.id}`} id='stepper_item' className={`${active < stepper.id && "pointer-events-none"} flex gap-3 items-center relative cursor-pointer w-max`}>
-                                <div className={`${active == stepper.id ? 'bg-[#333333] text-white' : 'text-[#33333] bg-white'} flex justify-center z-[9999] items-center w-9 h-9  px-3 py-1 rounded-[50%]  border-2 border-solid border-black`}>
+                                <div className={`${active == stepper.id ? 'bg-[#333333] text-white' : 'text-[#33333] bg-white'} flex justify-center z-10 items-center w-9 h-9  px-3 py-1 rounded-[50%]  border-2 border-solid border-black`}>
                                     {active == stepper.id ? <GiSandsOfTime className="text-2xl absolute" /> : (
                                         <>
                                             {active > stepper.id ? <IoCheckmark className="text-2xl absolute" /> : <span className='pt-[2.5px] text-sm'>{stepper.id}</span>}
                                         </>
                                     )}
                                 </div>
-                                <p className='text-[#444444] text-sm font-vazir font-light '>{stepper.title}</p>
+                                <p className='text-[#000000] text-sm font-vazir font-light '>{stepper.title}</p>
                             </Link>
                         ))}
                     </ul>
-                </AccordionHeader>
+                </div>
 
-            </Accordion>
+            </div>
         </>
 
     )
