364e81d88725b663d53d89362c29a1d301e04e3d
KasraMg <kasragithub7@gmail.com>
2024-07-08 01:20:55 +0330

    add inputOtp shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,7 @@ const config = {
     './node_modules/shadcn/dist/**/*.{js,ts,jsx,tsx}'
   ],
   prefix: "",
-  theme: {
-   
+  theme: { 
     container: {
       center: true,
       padding: "2rem",
@@ -19,11 +18,15 @@ const config = {
         "2xl": "1400px",
       },
     },
-    extend: {
+    extend: {  
       colors: {
         customYellow: '#f0c807', // مثال از رنگ سبز تیره
       },
       keyframes: {
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -34,6 +37,7 @@ const config = {
         },
       },
       animation: {
+        "caret-blink": "caret-blink 1.25s ease-out infinite", 
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
